5689137c17e768aacc98121bc18e58846af74605
NRK <nrk@disroot.org>
2024-11-20 12:00:31 +0000

    guard against multiple includes

--- chibihash64.h
+++ chibihash64.h
@@ -5,6 +5,7 @@
 //
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <https://unlicense.org/>
+#pragma once
 #include <stdint.h>
 #include <stddef.h>
 
