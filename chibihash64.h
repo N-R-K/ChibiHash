080f8b33ce70d1872e64b4ca313222c63fa587f5
NRK <nrk@disroot.org>
2024-11-29 15:53:51 +0000

    portable header guard

--- chibihash64.h
+++ chibihash64.h
@@ -1,10 +1,11 @@
+#ifndef CHIBIHASH64__HGUARD
+#define CHIBIHASH64__HGUARD
 // small, fast 64 bit hash function (version 2).
 //
 // https://github.com/N-R-K/ChibiHash
 //
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <https://unlicense.org/>
-#pragma once
 #include <stdint.h>
 #include <stddef.h>
 
@@ -77,3 +78,5 @@ chibihash64(const void *keyIn, ptrdiff_t len, uint64_t seed)
 
 	return x;
 }
+
+#endif // CHIBIHASH64__HGUARD
